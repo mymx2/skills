aee8ef361fbf18cc22b73945220e8e688eff9cf1
mymx2 <32814920+mymx2@users.noreply.github.com>
2026-05-06 17:58:35 +0800

    fix: language

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite-plus";
 
+const ignorePatterns = [
+  "pnpm-workspace.yaml",
+  "**/*-lock.*",
+  ".editorconfig",
+  ".gitattributes",
+  ".gitmodules",
+];
+
 export default defineConfig({
   lint: {
     options: {
       typeAware: true,
       typeCheck: true,
     },
+    ignorePatterns: [...ignorePatterns],
   },
   fmt: {},
   staged: {
